c3ecd412283f9590ba6f629245b9079504730035
Neiroromo <akkovl@ya.ru>
2020-12-10 03:38:57 +0800

    refactor: add changes to code
    изменил некоторые файлы, чтобы у меня запускался live server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import APIBehavior from './js/API.behavior.js';
-import './css/main.css';
-import './css/reset.css';
+// import './css/main.css';
+// import './css/reset.css';
 
 (async function () {
   await APIBehavior.getData();
