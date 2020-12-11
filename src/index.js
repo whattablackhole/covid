7594baa1736cb419347389ce39e318e4c0f4f2d6
Neiroromo <akkovl@ya.ru>
2020-12-12 00:58:30 +0800

    feat: done search
    Так же изменил:
    main.css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import APIBehavior from './js/API.behavior.js';
 import countriesList from './js/countries-list.create.js';
+import searchBehavior from './js/search.behavior.js'
 // import './css/main.css';
 // import './css/reset.css';
 
 (async function () {
   await APIBehavior.getData();
   countriesList.create();
+  searchBehavior.addOnChangeListener();
 })();
 
---
+++ src/js/search.behavior.js
@@ -0,0 +1,26 @@
+const searchBehavior = {
+    addOnChangeListener(){
+        let input = document.querySelector('.countries__input')
+        input.addEventListener('input',()=>{
+            this.search(input)
+        } )
+    },
+
+    search(input){
+        let value = input.value.toLowerCase()
+        if (value == '') value = '*'
+        let list = document.querySelectorAll('.countries__list li')
+        let regex = new RegExp(`${value}`);
+        
+        list.forEach(element => {
+            let countryInListName = element.childNodes[0].childNodes[2].innerHTML
+            if (regex.test(countryInListName.toLowerCase())){
+                element.classList.remove('hide')
+            } else {
+                element.classList.add('hide')
+            }
+        });
+    }
+}
+
+export default searchBehavior;
